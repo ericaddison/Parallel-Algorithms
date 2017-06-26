32da35ef672c193e512cd7dc87f1d162f7b1fb01
Eric Addison <ericaddison@gmail.com>
2017-06-26 17:02:37 -0500

    Moved p2a and p2b into separate files

---
+++ hw/eric/hw2/p2/p2a/src/p2.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+#include <sys/time.h>
+#include <cuda_runtime.h>
+#include "deviceFunctions.h"
+
+#define MIN(x,y) ((x<y)?x:y)
+#define MAX(x,y) ((x>y)?x:y)
+
+
+// p2a.cu functions
+__global__ void range_count_kernel_global(int * count, int * B, int * A, int n);
+__global__ void reduce_add_kernel_global(int * B, int * A, int n);
+
+
+// p2b.cu functions
+__global__ void range_count_kernel_shared(int * count, int * A, int n);
+__global__ void reduce_add_kernel_shared(int * B, int * A, int n);
+
+
--- hw/eric/hw2/p2/p2a/src/p2b.cu
+++ hw/eric/hw2/p2/p2a/src/p2b.cu
@@ -1,17 +1,17 @@
 //***********************************************
-// Problem 2a functions: use global memory
+// Problem 2b functions: use shared memory
 
 #include "p2.h"
 
-__global__ void range_count_kernel_global(int * count, int * B, int * A, int n)
+__global__ void range_count_kernel_shared(int * count, int * A, int n)
 {
+	extern __shared__ int sdata[];
     int myId = threadIdx.x + blockIdx.x * blockDim.x;
+    int tid = threadIdx.x;
 	int rangeBin = blockIdx.y;
-	int * C = B + n*rangeBin;
  
 	// create indicator array
-	if(myId<n)
-		C[myId] = ((A[myId]/100)==rangeBin);
+	sdata[tid] = ((A[myId]/100)==rangeBin);
 	__syncthreads();
 
 	// reduce indicator array
@@ -19,34 +19,36 @@ __global__ void range_count_kernel_global(int * count, int * B, int * A, int n)
 	for(int s=n2/2; s > 0; s>>=1)
 	{
 		if( d_checkReduceIndex(myId, s, n) )
-			C[myId] += C[myId+s];
+			sdata[tid] += sdata[tid+s];
 		__syncthreads();
 	}
 
 	// thread 0 write result
-	if(threadIdx.x==0)
-		count[blockIdx.x + gridDim.x*rangeBin] = C[myId];
+	if(tid==0)
+		count[blockIdx.x + gridDim.x*rangeBin] = sdata[tid];
 }
 
 
-__global__ void reduce_add_kernel_global(int * B, int * A, int n)
+__global__ void reduce_add_kernel_shared(int * B, int * A, int n)
 {
+	extern __shared__ int sdata[];
     int myId = threadIdx.x + blockIdx.x * blockDim.x;
+    int tid = threadIdx.x;
 	int rangeBin = blockIdx.y;
-	int * C = A + n*rangeBin;
+
+	// copy value from global to shared memory
+	sdata[tid] = A[myId + n*rangeBin];
 
 	// reduce block array
 	int n2 = d_next_pow2(blockDim.x);
 	for(int s=n2/2; s > 0; s>>=1)
 	{
 		if( d_checkReduceIndex(myId, s, n) )
-			C[myId] += C[myId+s];
+			sdata[tid] += sdata[tid+s];
 		__syncthreads();
 	}
 
 	// thread 0 write result
-	if(threadIdx.x==0)
-		B[blockIdx.x + rangeBin*gridDim.x] = C[myId];
+	if(tid==0)
+		B[blockIdx.x + rangeBin*gridDim.x] = sdata[tid];
 }
-
-
