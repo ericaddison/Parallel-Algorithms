4e45e243f54e4d3215c572fccead828319dede30
Eric Addison <ericaddison@gmail.com>
2017-06-29 16:55:20 -0500

    Maybe p3 working???

--- hw/eric/hw2/p3/src/p3.cu
+++ hw/eric/hw2/p3/src/p3.cu
@@ -4,7 +4,7 @@ extern "C"
 	#include "randomInts.h"
 }
 
-#define MAX_THREADS 8
+#define MAX_THREADS 4
 
 enum repeatCheck { check, noCheck };
 
@@ -60,14 +60,19 @@ __global__ void radix_sort_kernel(int *A, int n, int nDigits)
 
 
 // this will merge two pieces of array A of size n into the first 2n entries of d_out
-__global__ void parallel_merge_kernel(int *d_out, int *A, int n)
+__global__ void parallel_merge_kernel(int *d_out, int *A, int treeLevel)
 {
-	int tid = threadIdx.x;
-	int myInd = tid;
+
+    int mergeID = blockIdx.x/treeLevel;
+    int n = treeLevel*blockDim.x;    
+
+	int offset = (mergeID/2)*n;
+	int tid = threadIdx.x + offset + (blockIdx.x%treeLevel)*blockDim.x;
+	A = A+offset;
 	int *B = A+n;
-	
+
 	// swap A and B if this is blockIdx.y==1
-	if(blockIdx.y==1)
+	if(mergeID%2)
 	{
 		int *C = A;
 		A = B;
@@ -75,17 +80,17 @@ __global__ void parallel_merge_kernel(int *d_out, int *A, int n)
 	}
 		
 	int otherInd = d_binary_search(B, A[tid], n);
-	int mergedIndex = myInd + otherInd;
+	int mergedIndex = tid + otherInd;
 	int nRepeats=0;
 
 	// sensitive to repeated elements if blockIdx.y==1
-	if(blockIdx.y==1)
+	if(mergeID%2)
 	{
 	 	nRepeats = otherInd - d_binary_search(B,A[tid]-1,n);		
 		mergedIndex -= nRepeats;
 	}
 
-	d_out[mergedIndex] = A[tid];	
+	d_out[mergedIndex+offset] = A[tid];	
 }
 
 
@@ -102,11 +107,16 @@ int main()
     int n = (int)pow(2,exp); 
 	n = 10;
 	
+	printf("MAX_THREADS = %d\n",MAX_THREADS);
+	printf("n = %d\n",n);
+
+
 	// pad array if less than a power of 2
-	int* h_A = (int*)malloc(n*(sizeof(int)));
+	int np2 = next_pow2(n);
+	int* h_A = (int*)malloc(np2*(sizeof(int)));
 
 	// make test array
-	writeRandomFile(n, "inp.txt");
+	//writeRandomFile(n, "inp.txt");
 	readIntsFromFile("inp.txt",n,h_A);
 	//int h_A[] = {3, 5, 5, 2, 5, 1, 7, 7};
 
@@ -115,8 +125,8 @@ int main()
 	for(int i=0; i<n; i++)
 		printf("%d, ",h_A[i]);
 	printf("\n");
+
 	
-	int np2 = next_pow2(n);
 	int *d_A, *d_B;
 	cudaMalloc((int**)&d_A, np2*sizeof(int));
 	cudaMalloc((int**)&d_B, np2*sizeof(int));
@@ -129,11 +139,50 @@ int main()
 	radix_sort_kernel<<<nBlocks,threadsPerBlock,2*threadsPerBlock*sizeof(int)>>>(d_A, threadsPerBlock, nDigits);
 	cudaThreadSynchronize();
 
+
+	// print A
+	printf("A: ");
+		cudaMemcpy(h_A, d_A, np2*sizeof(int), cudaMemcpyDeviceToHost);
+	for(int i=0; i<np2; i++)
+	{
+		if(i%MAX_THREADS==0 && i>0)
+			printf(" | ");
+		printf("%d, ",h_A[i]);
+	}
+	printf("\n");
+
 	if(nBlocks>1)
 	{
-		parallel_merge_kernel<<<dim3(1,2),threadsPerBlock>>>(d_B,d_A,threadsPerBlock);
-		cudaThreadSynchronize();
-		cudaMemcpy(h_A, d_B+(np2-n), n*sizeof(int), cudaMemcpyDeviceToHost);
+		int cnt=1;
+		int nMerges = nBlocks;
+		while(nMerges>1)
+		{
+			printf("merging round %d\n",cnt, nBlocks);
+			parallel_merge_kernel<<<nBlocks,threadsPerBlock>>>(d_B,d_A,cnt);
+			cudaThreadSynchronize();
+
+	// print B
+			printf("B: ");
+			cudaMemcpy(h_A, d_B, np2*sizeof(int), cudaMemcpyDeviceToHost);
+			for(int i=0; i<np2; i++)
+			{
+				if(i%(MAX_THREADS*cnt*2)==0 && i>0)
+					printf(" | ");
+				printf("%d, ",h_A[i]);
+
+			}
+			printf("\n");
+			cnt*=2;
+			nMerges /= 2;
+
+            // swap A and B
+            int *d_C = d_A;
+            d_A = d_B;
+            d_B = d_C;
+
+		}
+		//cudaMemcpy(h_A, d_B+(np2-n), n*sizeof(int), cudaMemcpyDeviceToHost);
+		cudaMemcpy(h_A, d_A, np2*sizeof(int), cudaMemcpyDeviceToHost);
 	}
 	else
 	{
@@ -142,9 +191,13 @@ int main()
 
 
 
-	for(int i=0; i<n; i++)
+	// print A
+	printf("A: ");
+		cudaMemcpy(h_A, d_A, np2*sizeof(int), cudaMemcpyDeviceToHost);
+	for(int i=0; i<np2; i++)
 		printf("%d, ",h_A[i]);
 	printf("\n");
+
 
 	printf("Array is %s sorted\n", (checkSorted(h_A,n)?"\b":"NOT"));
 
